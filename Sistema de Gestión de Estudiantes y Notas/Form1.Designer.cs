d0808d1969956787531ebf92b90d5f90d5285cd8
Orlando <yandelmartinez864@gmail.com>
2025-03-10 20:47:21 -0400

    Revert "Puse un boton"
    This reverts commit 661257bd9cd8d7a0329d1c19fc56caf50a7dc1f6.

--- Sistema de Gestión de Estudiantes y Notas/Form1.Designer.cs
+++ Sistema de Gestión de Estudiantes y Notas/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.dgvEstudiantes = new System.Windows.Forms.DataGridView();
             this.btnCargar = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEstudiantes)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,21 +52,11 @@
             this.btnCargar.UseVisualStyleBackColor = false;
             this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(489, 371);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 49);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnCargar);
             this.Controls.Add(this.dgvEstudiantes);
             this.Name = "Form1";
@@ -81,7 +70,6 @@
 
         private System.Windows.Forms.DataGridView dgvEstudiantes;
         private System.Windows.Forms.Button btnCargar;
-        private System.Windows.Forms.Button button1;
     }
 }
 
