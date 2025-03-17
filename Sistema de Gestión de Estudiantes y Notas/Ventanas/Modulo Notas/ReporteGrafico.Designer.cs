beea9255582cc5e1f47acda5b8f91eb062039c73
Orlando <yandelmartinez864@gmail.com>
2025-03-16 22:24:58 -0400

    Implementando menu contextual

---
+++ Sistema de Gestión de Estudiantes y Notas/Ventanas/Modulo Notas/ReporteGrafico.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Sistema_de_Gestión_de_Estudiantes_y_Notas.Ventanas.Modulo_Notas
+{
+    partial class ReporteGrafico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartReporte = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartReporte)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartReporte
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartReporte.ChartAreas.Add(chartArea1);
+            this.chartReporte.Cursor = System.Windows.Forms.Cursors.No;
+            legend1.Name = "Legend1";
+            this.chartReporte.Legends.Add(legend1);
+            this.chartReporte.Location = new System.Drawing.Point(1, 0);
+            this.chartReporte.Name = "chartReporte";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartReporte.Series.Add(series1);
+            this.chartReporte.Size = new System.Drawing.Size(797, 447);
+            this.chartReporte.TabIndex = 0;
+            this.chartReporte.Text = "chart1";
+            // 
+            // ReporteGrafico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chartReporte);
+            this.Name = "ReporteGrafico";
+            this.Text = "ReporteGrafico";
+            ((System.ComponentModel.ISupportInitialize)(this.chartReporte)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartReporte;
+    }
+}
