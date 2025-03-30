93a7281be07701b247e999faa36eb2c452d2abbe
Orlando <yandelmartinez864@gmail.com>
2025-03-29 21:25:00 -0400

    Creacion de modulos

--- Sistema de Gestión de Estudiantes y Notas/Ventanas/Modulo Materia/Materia.Designer.cs
+++ Sistema de Gestión de Estudiantes y Notas/Ventanas/Modulo Materia/Materia.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvMaterias = new System.Windows.Forms.DataGridView();
+            this.txtMateria = new System.Windows.Forms.TextBox();
+            this.cmbMaestro = new System.Windows.Forms.ComboBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaterias)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMaterias
+            // 
+            this.dgvMaterias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMaterias.Location = new System.Drawing.Point(51, 61);
+            this.dgvMaterias.Name = "dgvMaterias";
+            this.dgvMaterias.Size = new System.Drawing.Size(177, 234);
+            this.dgvMaterias.TabIndex = 0;
+            // 
+            // txtMateria
+            // 
+            this.txtMateria.Location = new System.Drawing.Point(282, 109);
+            this.txtMateria.Name = "txtMateria";
+            this.txtMateria.Size = new System.Drawing.Size(121, 20);
+            this.txtMateria.TabIndex = 1;
+            // 
+            // cmbMaestro
+            // 
+            this.cmbMaestro.FormattingEnabled = true;
+            this.cmbMaestro.Location = new System.Drawing.Point(282, 158);
+            this.cmbMaestro.Name = "cmbMaestro";
+            this.cmbMaestro.Size = new System.Drawing.Size(121, 21);
+            this.cmbMaestro.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(272, 218);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(364, 218);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // Materia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(474, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.cmbMaestro);
+            this.Controls.Add(this.txtMateria);
+            this.Controls.Add(this.dgvMaterias);
+            this.Name = "Materia";
             this.Text = "Materia";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaterias)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvMaterias;
+        private System.Windows.Forms.TextBox txtMateria;
+        private System.Windows.Forms.ComboBox cmbMaestro;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
     }
 }
