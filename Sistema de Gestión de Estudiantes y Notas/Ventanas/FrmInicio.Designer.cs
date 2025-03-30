9ade98416c39618b2f3bfb4332cd47a1826c91de
Orlando <yandelmartinez864@gmail.com>
2025-03-29 20:29:00 -0400

    Login

--- Sistema de Gestión de Estudiantes y Notas/Ventanas/FrmInicio.Designer.cs
+++ Sistema de Gestión de Estudiantes y Notas/Ventanas/FrmInicio.Designer.cs
@@ -29,34 +29,55 @@
         private void InitializeComponent()
         {
             this.btnEntrar = new System.Windows.Forms.Button();
+            this.texusuario = new System.Windows.Forms.TextBox();
+            this.texcontrasena = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnEntrar
             // 
             this.btnEntrar.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.btnEntrar.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnEntrar.Location = new System.Drawing.Point(217, 114);
+            this.btnEntrar.Location = new System.Drawing.Point(102, 217);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(216, 135);
+            this.btnEntrar.Size = new System.Drawing.Size(110, 33);
             this.btnEntrar.TabIndex = 0;
             this.btnEntrar.Text = "Entrar";
             this.btnEntrar.UseVisualStyleBackColor = false;
             this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
             // 
+            // texusuario
+            // 
+            this.texusuario.Location = new System.Drawing.Point(102, 152);
+            this.texusuario.Name = "texusuario";
+            this.texusuario.Size = new System.Drawing.Size(110, 20);
+            this.texusuario.TabIndex = 1;
+            // 
+            // texcontrasena
+            // 
+            this.texcontrasena.Location = new System.Drawing.Point(102, 191);
+            this.texcontrasena.Name = "texcontrasena";
+            this.texcontrasena.Size = new System.Drawing.Size(110, 20);
+            this.texcontrasena.TabIndex = 2;
+            // 
             // FrmInicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 361);
+            this.ClientSize = new System.Drawing.Size(296, 361);
+            this.Controls.Add(this.texcontrasena);
+            this.Controls.Add(this.texusuario);
             this.Controls.Add(this.btnEntrar);
             this.Name = "FrmInicio";
             this.Text = "FrmInicio";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.TextBox texusuario;
+        private System.Windows.Forms.TextBox texcontrasena;
     }
 }
